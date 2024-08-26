1205410285f0ebf764a92961d25de8c4a719b14c
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2024-08-26 19:03:32 +0500

    bugfix: now character.cs uses input parameters instead of mouse input to rotate

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -5,8 +5,8 @@ public class Character : MonoBehaviour
     [SerializeField] private float speed;
     [SerializeField] private CharacterController characterController;
     [SerializeField] private Transform head;
-    private float _minHeadRotationAngle = -45.0f;
-    private float _maxHeadRotationAngle = 45.0f;
+    private float _minHeadRotationAngle = -60.0f;
+    private float _maxHeadRotationAngle = 60.0f;
     private float _headRotationAngle = 0;
 
     public void Move(Vector2 input)
@@ -20,11 +20,8 @@ public class Character : MonoBehaviour
     public void Rotate(Vector2 input)
     {
         transform.Rotate(0, input.x, 0);
-        _headRotationAngle -= Input.GetAxis("Mouse Y");
+        _headRotationAngle -= input.y;
         _headRotationAngle = Mathf.Clamp(_headRotationAngle, _minHeadRotationAngle, _maxHeadRotationAngle);
-        float delta = Input.GetAxis("Mouse X");
-        float rotationY = transform.localEulerAngles.y + delta;
-        transform.localEulerAngles = new Vector3(0, rotationY, 0);
         head.transform.localEulerAngles = new Vector3(_headRotationAngle, 0, 0);
     }
 }
--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -3,12 +3,12 @@ using UnityEngine;
 public class PlayerInput : MonoBehaviour
 {
     [SerializeField] private Character character;
-    public float sensitivityHor = 9.0f;
-    public float sensitivityVert = 9.0f;
+    [SerializeField] private float sensitivityHorizontal;
+    [SerializeField] private float sensitivityVertical;
 
     void Update()
     {
         character.Move(new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")));
-        character.Rotate(new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y")));
+        character.Rotate(new Vector2(Input.GetAxis("Mouse X") * sensitivityHorizontal, Input.GetAxis("Mouse Y") * sensitivityVertical));
     }
 }
